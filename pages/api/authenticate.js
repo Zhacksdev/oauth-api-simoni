c13c1987b570d6c852007dd9dcdcdf70a2543b5d
Zhacksdev <muhammadzacky@student.telkomuniversity.ac.id>
2025-10-16 09:50:28 +0700

    add sales_invoice

--- pages/api/authenticate.js
+++ pages/api/authenticate.js
@@ -3,7 +3,7 @@ export default function handler(req, res) {
   const client_id = process.env.ACCURATE_CLIENT_ID;
   const redirect_uri = process.env.ACCURATE_REDIRECT_URI;
 
-  const authUrl = `https://account.accurate.id/oauth/authorize?response_type=code&client_id=${client_id}&redirect_uri=${redirect_uri}&scope=sales_receipt_view`;
+  const authUrl = `https://account.accurate.id/oauth/authorize?response_type=code&client_id=${client_id}&redirect_uri=${redirect_uri}&scope=sales_receipt_view_sales_invoice_view`;
 
   res.redirect(authUrl);
 }
