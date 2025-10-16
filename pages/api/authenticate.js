ef00832dd92c79aed338cfcf14536ca063937542
Zhacksdev <muhammadzacky@student.telkomuniversity.ac.id>
2025-10-16 09:58:12 +0700

    fix scopes

--- pages/api/authenticate.js
+++ pages/api/authenticate.js
@@ -3,7 +3,12 @@ export default function handler(req, res) {
   const client_id = process.env.ACCURATE_CLIENT_ID;
   const redirect_uri = process.env.ACCURATE_REDIRECT_URI;
 
-  const authUrl = `https://account.accurate.id/oauth/authorize?response_type=code&client_id=${client_id}&redirect_uri=${redirect_uri}&scope=sales_receipt_view_sales_invoice_view`;
+  const scopes = ["sales_receipt_view", "sales_invoice_view"];
+  const scopeParam = encodeURIComponent(scopes.join(" ")); // menjadi: sales_receipt_view%20sales_invoice_view
+
+  const authUrl = `https://account.accurate.id/oauth/authorize?response_type=code&client_id=${client_id}&redirect_uri=${encodeURIComponent(
+    redirect_uri
+  )}&scope=${scopeParam}`;
 
   res.redirect(authUrl);
 }
